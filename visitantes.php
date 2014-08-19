42c3a8449f72e9b857abe4eefa880a1b732cd9d4
dannypads <dannypads1@gmail.com>
2014-08-19 12:10:33 -0500

    arriba

---
+++ visitantes.php
@@ -0,0 +1,24 @@
+<?php
+
+ /**
+ * 
+ */
+ class vistantes 
+ {
+ 	
+ 	function __construct()
+ 	{
+
+ 	}
+
+ 	function get_visitante()
+ 	{
+
+ 	}
+
+ 	function set_visitante()
+ 	{
+ 		
+ 	}
+ }
+?>
